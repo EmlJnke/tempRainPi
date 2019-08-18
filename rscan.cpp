e0217258fc70058f3a3752df0536bade9949de4b
Emil J <emil.jahnke@icloud.com>
2019-08-18 19:22:18 +0200

    Sensor ID changed
    Changed the "String to recognize the sensor" to fit to the ID of my device, also removed one number because this would already be the "low battery bit"

--- rscan.cpp
+++ rscan.cpp
@@ -12,27 +12,29 @@
 static volatile int keepRunning = 1;
 
 // String to recognize the sensor
-char idst[] = "11111110011011001110\0";
+char idst[] = "1111111001100000101\0";
 
 char sign[200];
 char sipo = 0;
-int  temp = 0;
-int  rainl = 0;
-int  rainb = 0;
+int temp = 0;
+int rainl = 0;
+int rainb = 0;
 
-int  newbit(char bit);
+int newbit(char bit);
 
 /*
  * Signal handler to catch Ctrl-C to terminate the program safely.
  */
-void sigIntHandler(int dummy) {
+void sigIntHandler(int dummy)
+{
   keepRunning = 0;
-} 
+}
 
 /*
  * Thread to read the output of the receiver module.
  */
-PI_THREAD (decoderThread) {
+PI_THREAD(decoderThread)
+{
   piHiPri(50);
 
   int x;
@@ -41,21 +43,29 @@ PI_THREAD (decoderThread) {
   int hi = 0;
   int px = 0;
 
-  while (keepRunning) {
+  while (keepRunning)
+  {
     x = digitalRead(2);
 
     len++;
-    if (0 == x) {
+    if (0 == x)
+    {
       lo++;
-    } else {
+    }
+    else
+    {
       hi++;
     }
 
-    if (x != px && 0 == x) {
+    if (x != px && 0 == x)
+    {
       // slope high->low
-      if (hi > 3 && hi < 12) {
+      if (hi > 3 && hi < 12)
+      {
         newbit('0');
-      } else {
+      }
+      else
+      {
         newbit('1');
       }
       len = 1;
@@ -69,58 +79,67 @@ PI_THREAD (decoderThread) {
 }
 
 // Not the best decoder, but working...
-int newbit(char bit) {
-   if ((strlen(idst) - 1 > sipo) && (bit != idst[sipo])) {
-     // Not the sensor -> reset
-     sipo = 0;
-     temp = 0;
-     rainl = 0;
-     rainb = 0;
-     return 0; 
-   } 
-   sign[sipo] = bit;
-   if (sipo > 19 && sipo < 31) {
-     temp *= 2;
-     if (bit == '1') {
-       temp += 1;
-     }
-   }
-   if (sipo > 30 && sipo < 39) {
-     rainl *= 2;
-     if (bit == '1') {
-       rainl += 1;
-     }
-   }
-   if (sipo > 38 && sipo < 47) {
-     rainb *= 2;
-     if (bit == '1') {
-       rainb += 1;
-     }
-   }
-   if (sipo > 93) {
-     // Print the signal
-     printf("Signal found:\n%s\n  .   .   .   i   i ?ttttttttttrrrrrrrrRRRRRRRR   .   .   m   m   m   m   d   d   d   d   d   d\n", sign);
-     float rtemp = (temp - 400) / 10.0;
-     printf("Temp: %.1f°C\n", rtemp);
-     float rrain = (rainb * 256 + rainl) * 0.3;
-     printf("Rain: %.1f mm\n", rrain);
-     sipo = 0;
-     temp = 0;
-     rainl = 0;
-     rainb = 0;
-     return 0;
-   }
-   sipo++;
-   return 0;
-
+int newbit(char bit)
+{
+  if ((strlen(idst) - 1 > sipo) && (bit != idst[sipo]))
+  {
+    // Not the sensor -> reset
+    sipo = 0;
+    temp = 0;
+    rainl = 0;
+    rainb = 0;
+    return 0;
+  }
+  sign[sipo] = bit;
+  if (sipo > 19 && sipo < 31)
+  {
+    temp *= 2;
+    if (bit == '1')
+    {
+      temp += 1;
+    }
+  }
+  if (sipo > 30 && sipo < 39)
+  {
+    rainl *= 2;
+    if (bit == '1')
+    {
+      rainl += 1;
+    }
+  }
+  if (sipo > 38 && sipo < 47)
+  {
+    rainb *= 2;
+    if (bit == '1')
+    {
+      rainb += 1;
+    }
+  }
+  if (sipo > 93)
+  {
+    // Print the signal
+    printf("Signal found:\n%s\n  .   .   .   i   i ?ttttttttttrrrrrrrrRRRRRRRR   .   .   m   m   m   m   d   d   d   d   d   d\n", sign);
+    float rtemp = (temp - 400) / 10.0;
+    printf("Temp: %.1f°C\n", rtemp);
+    float rrain = (rainb * 256 + rainl) * 0.3;
+    printf("Rain: %.1f mm\n", rrain);
+    sipo = 0;
+    temp = 0;
+    rainl = 0;
+    rainb = 0;
+    return 0;
+  }
+  sipo++;
+  return 0;
 }
 
 /*
  * MAIN
  */
-int main() {
+int main()
+{
   wiringPiSetup();
-/*
+  /*
   BCM GPIO 27: DATA (IN) == WiPin 2
 */
   pinMode(2, INPUT);
@@ -130,7 +149,8 @@ int main() {
 
   piThreadCreate(decoderThread);
 
-  while (keepRunning) {
+  while (keepRunning)
+  {
     piLock(1);
     piUnlock(1);
     delay(100);
